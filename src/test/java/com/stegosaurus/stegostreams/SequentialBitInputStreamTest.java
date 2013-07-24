5bd5aa788fca59e8691c7b56febf28119cf398cd
Joe C <escozzia@gmail.com>
2013-07-23 22:27:56 -0400

    Retab a file

--- src/test/java/com/stegosaurus/stegostreams/SequentialBitInputStreamTest.java
+++ src/test/java/com/stegosaurus/stegostreams/SequentialBitInputStreamTest.java
@@ -8,30 +8,39 @@ import java.util.Arrays;
 import org.junit.Test;
 import com.stegosaurus.stegostreams.SequentialBitInputStream;
 
+/**
+ * Tests the sequential bit inputstream class.
+ */
 public class SequentialBitInputStreamTest {
 
-	@Test
-	public void testRead() {
-		byte[] arg = { 0b01011101 };
-		SequentialBitInputStream st = new SequentialBitInputStream(arg);
-		byte[] expected = { 0, 1, 0, 1, 1, 1, 0, 1 };
-		byte[] retval = new byte[8];
-		try {
-			for (int i = 0; i < retval.length; i++) {
-				retval[i] = (byte) st.read();
-			}
-			st.close();
-		} catch (IOException e) {
-			fail("Unexpected exception: " + e);
-		}
-		assertTrue("Wrong return value from read",
-				Arrays.equals(retval, expected));
-	}
+  /**
+   * Test the read method.
+   */
+  @Test
+  public void testRead() {
+    byte[] arg = { 0b01011101 };
+    SequentialBitInputStream st = new SequentialBitInputStream(arg);
+    byte[] expected = { 0, 1, 0, 1, 1, 1, 0, 1 };
+    byte[] retval = new byte[8];
+    try {
+      for (int i = 0; i < retval.length; i++) {
+        retval[i] = (byte) st.read();
+      }
+      st.close();
+    } catch (IOException e) {
+      fail("Unexpected exception: " + e);
+    }
+    assertTrue("Wrong return value from read",
+        Arrays.equals(retval, expected));
+  }
 
+  /**
+   * Test the skipToEndOfByte method.
+   */
   @Test
   public void testSkipToEndOfByte() {
     byte[] arg = { 0b01001111, 0b00001101 };
-		BitInputStream st = new SequentialBitInputStream(arg);
+    BitInputStream st = new SequentialBitInputStream(arg);
     for(int i = 0; i < 4; i++) {
       st.read();
     }
