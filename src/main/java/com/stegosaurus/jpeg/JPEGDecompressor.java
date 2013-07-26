3b09f71310aa8b6c52085e32711595e0ee02d7f6
Joe C <escozzia@gmail.com>
2013-07-25 21:03:54 -0400

    Make the decompressor use trove, and reorder the zig-zagged coefficients

--- src/main/java/com/stegosaurus/jpeg/JPEGDecompressor.java
+++ src/main/java/com/stegosaurus/jpeg/JPEGDecompressor.java
@@ -1,15 +1,17 @@
 package com.stegosaurus.jpeg;
 
+import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TIntArrayList;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.lang3.ArrayUtils;
 
 import com.stegosaurus.huffman.HuffmanDecoder;
 import com.stegosaurus.stegostreams.BitInputStream;
 import com.stegosaurus.stegutils.NumUtils;
+import com.stegosaurus.stegutils.ZigZag;
 
 public class JPEGDecompressor extends JPEGProcessor {
   /**
@@ -59,15 +61,17 @@ public class JPEGDecompressor extends JPEGProcessor {
   }
 
   /**
-   * Decode 63 AC components and place them in the output list given.
+   * Decode 63 AC components and place them in the output array given,
+   * starting at index 1, to account for a DC component being present.
    * @param decoder the huffman decoder to use for the components
    * @param in the input stream where the data is to be found
-   * @param out the list to place the decoded components into
+   * @param out the byte array where the data is to be stored
    */
   private void decodeACs(HuffmanDecoder decoder, BitInputStream in,
-    List<Integer> out) throws IOException {
-    int i = 0;
-    while(i < 63) {
+                          int[] out) throws IOException {
+    int i = 1;
+    final int total = 64;
+    while(i < total) {
       byte symbol1 = decoder.decodeNext(in);
       byte magnitude = (byte) (symbol1 & (byte) 0xF0);
       byte zeroRun = (byte) (symbol1 * 0x0F);
@@ -76,22 +80,22 @@ public class JPEGDecompressor extends JPEGProcessor {
           /* We've encountered a run of 16 zeroes. Emplace them, and increment
            * our counter */
           for(int j = 0; j < 16; j++, i++) {
-            out.add(0);
+            out[i] = 0;
           }
         } else if (zeroRun == 0) {
           /* We've encountered the end of block. Push i to 63 and write any
            * remaining 0s */
-          for(; i < 63; i++) {
-            out.add(0);
+          for(; i < total; i++) {
+            out[i] = 0;
           }
         }
       } else {
         /* First emplace the preceding run of zeroes */
         for(int j = 0; j < zeroRun; j++, i++) {
-          out.add(0);
+          out[i] = 0;
         }
         byte[] extra = new byte[magnitude];
-        out.add(extend(NumUtils.intFromBits(extra), magnitude));
+        out[i] = extend(NumUtils.intFromBits(extra), magnitude);
       }
     }
   }
@@ -117,7 +121,7 @@ public class JPEGDecompressor extends JPEGProcessor {
    * @param input the data to decompress.
    * @param output the list into which the data will be placed.
    */
-  private void decompress(Scan scan, byte[] input, List<Integer> output)
+  private void decompress(Scan scan, byte[] input, TIntList output)
       throws IOException {
     /* TODO Waste of good time. There must be a better way */
     if(input[0] == (byte) 0xFF && JPEGConstants.isRSTMarker(input[1])) {
@@ -134,8 +138,10 @@ public class JPEGDecompressor extends JPEGProcessor {
             int acTable = (scan.getTableId(cmp) & 0x0F) | 0x10;
             int dc = decodeDC(scan.getDecoder(dcTable), in, lastDCs[cmp]);
             lastDCs[cmp] = dc;
-            output.add(dc);
-            decodeACs(scan.getDecoder(acTable), in, output);
+            int[] coeffs = new int[64];
+            coeffs[0] = dc;
+            decodeACs(scan.getDecoder(acTable), in, coeffs);
+            output.addAll(ZigZag.zigZagToSequential(coeffs));
           }
         }
       }
@@ -150,8 +156,8 @@ public class JPEGDecompressor extends JPEGProcessor {
    */
   @Override
   protected Scan process(Scan scan) {
-    /* Every coefficient is going to become an int in here, so there we are */
-    List<Integer> data = new ArrayList<Integer>(scan.getCoefficientCount());
+    /* Every coefficient is going to become an int in here */
+    TIntList data = new TIntArrayList(scan.getCoefficientCount());
     try {
       for(byte[] piece : scan) {
         decompress(scan, piece, data);
@@ -164,10 +170,7 @@ public class JPEGDecompressor extends JPEGProcessor {
        */
       throw new IllegalArgumentException("Scan data caused exception", ioe);
     }
-    /* XXX This statement kinda sucks. */
-    scan.setData(NumUtils.
-      byteArrayFromIntArray(ArrayUtils.
-        toPrimitive(data.toArray(new Integer[data.size()]))));
+    scan.setData(NumUtils.byteArrayFromIntArray(data.toArray()));
     return scan;
   }
 }
