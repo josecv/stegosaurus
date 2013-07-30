db2406979d824ff5b02fec5639bbff7454ecfa2f
Joe C <escozzia@gmail.com>
2013-07-29 20:07:56 -0400

    Some refactoring

--- src/main/java/com/stegosaurus/steganographers/Steganographer.java
+++ src/main/java/com/stegosaurus/steganographers/Steganographer.java
@@ -1,22 +1,25 @@
 package com.stegosaurus.steganographers;
 
 import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
 
 import com.stegosaurus.steganographers.coders.Hider;
 import com.stegosaurus.stegostreams.BitInputStream;
+import com.stegosaurus.stegutils.NumUtils;
 
 
-/*
- * TODO: Make some kind of abstract Stegano-thing and then have it have two
- * abstract children: Stego and DeStego, and etc.
- */
 /**
  * Mediates with a Hider to hide a payload in a carrier. Not re-usable.
- *
- * @author joe
+ * At no time attempts to preserve any already encoded data.
  */
 public class Steganographer {
     
+  /**
+   * The hider in use.
+   */
   private Hider hider;
 
   /**
@@ -29,13 +32,47 @@ public class Steganographer {
   }
 
   /**
-   * Hide the data given into this steganographer's target. Notice that this
-   * makes no effort to preserve any previous data that may have been hidden
-   * there.
-   *
+   * Hide the data from the stream given into this steganographer's target.
+   * Note that it is not closed.
+   * @param stream the data to hide, as a stream.
+   * @return a byte array, being the carrier containing the data.
+   * @throws IOException if the stream given produces an error, or on
+   *    hider failure
+   */
+  public byte[] hide(InputStream stream) throws IOException {
+    return hide(IOUtils.toByteArray(stream));
+  }
+
+  /**
+   * Hide the message given into this steganographer's target.
+   * @param message the data to hide, as a string.
+   * @return a byte array, being the carrier containing the data.
+   * @throws IOException on hider failure
+   */
+  public byte[] hide(String message) throws IOException {
+    return hide(message.getBytes());
+  }
+
+  /**
+   * Hide the data given into this steganographer's target.
+   * @param data the data to hide.
+   * @return a byte array, being the carrier containing the data.
+   * @throws IOException on hider failure
+   */
+  public byte[] hide(byte[] data) throws IOException {
+    byte[] encodeData = NumUtils.byteArrayFromInt(data.length);
+    encodeData = ArrayUtils.addAll(encodeData, data);
+    BitInputStream in = new BitInputStream(encodeData);
+    hider.hide(in, in.available());
+    in.close();
+    return hider.close();
+  }
+
+  /**
+   * Hide the data given into this steganographer's target.
    * @param datastream the data to hide, as a stream of bits.
-   * @return a byte array with the hidden data.
-   * @throws IOException
+   * @return a byte array, being the carrier containing the data.
+   * @throws IOException on hider failure
    */
   public byte[] hide(BitInputStream datastream) throws IOException {
     this.hider.hide(datastream, datastream.available());
