fba4fd9821010c1f5703abba2a13d510147297d8
Joe C <escozzia@gmail.com>
2013-09-15 23:10:08 -0400

    Some efforts to map the JOCTET arrays to java objects.
    This is still ugly as heck, but at least it should be workable now.

---
+++ src/main/java/com/stegosaurus/stegutils/NativeUtils.java
@@ -0,0 +1,63 @@
+package com.stegosaurus.stegutils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import com.stegosaurus.cpp.JoctetArray;
+
+/**
+ * Smooths out some of the rougher aspects of the glue between the native
+ * libstegosaurus and the Java portions of Stegosaurus.
+ * For the most part, this involves creating and populating arrays and
+ * other such things.
+ */
+public final class NativeUtils {
+  /**
+   * Private CTOR.
+   */
+  private NativeUtils() { }
+
+  /**
+   * A custom JoctetArray, extended to the SWIG-generated one, but keeps
+   * length information handy.
+   */
+  public static class StegJoctetArray extends JoctetArray {
+    /**
+     * The length of the array.
+     */
+    private int length;
+    /**
+     * CTOR.
+     * @param nelements the size of the array.
+     */
+    public StegJoctetArray(int nelements) {
+      super(nelements);
+      length = nelements;
+    }
+    /**
+     * Get the length.
+     * @return the length
+     */
+    public int length() {
+      return length;
+    }
+  }
+
+  /**
+   * Read an input stream into a JoctetArray and return it.
+   * @param in the input stream to read.
+   * @return the StegJoctetArray corresponding to it.
+   * @throws IOException on read error.
+   */
+  public static StegJoctetArray readInputStream(InputStream in)
+    throws IOException {
+    byte[] b = IOUtils.toByteArray(in);
+    StegJoctetArray retval = new StegJoctetArray(b.length);
+    for(int i = 0; i < b.length; i++) {
+      retval.setitem(i, b[i]);
+    }
+    return retval;
+  }
+}
--- src/main/stegosaurus.i
+++ src/main/stegosaurus.i
@@ -9,6 +9,11 @@
 #include "cpp/coefficient_accessor.h"
 #include "cpp/jpeg_context.h"
 %}
+/*%include "arrays_java.i"
+%apply signed char[] {signed char *};*/
+%apply signed char {JOCTET};
+%include "carrays.i"
+%array_class(JOCTET, JoctetArray);
 /* Now tell swig to parse the header files */
 %include "cpp/jpeg_component.h"
 %include "cpp/jpeg_image.h"
