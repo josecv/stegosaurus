2141fad7db6a7e391b3960949d6ce87267d6f3c0
Joe C <escozzia@gmail.com>
2013-09-19 00:17:32 -0400

    A class to better deal with the specifics of permuting image coefs.
    The ImagePermuter is just a small utility class that handles iterating
    over rearranged DCT coefficients, while skipping those that are
    inappropriate for embedding.

---
+++ src/main/java/com/stegosaurus/steganographers/ImagePermuter.java
@@ -0,0 +1,84 @@
+package com.stegosaurus.steganographers;
+
+import gnu.trove.procedure.TIntIntProcedure;
+import gnu.trove.procedure.TIntProcedure;
+
+import java.util.BitSet;
+import java.util.Random;
+
+import com.stegosaurus.cpp.CoefficientAccessor;
+import com.stegosaurus.crypt.Permutation;
+
+/**
+ * Given a coefficient accessor and a permutation for it, will walk through
+ * the permutation and execute a callback for every value that is not a DC
+ * coefficient and not a 0 or a 1.
+ * The permuter keeps track of already accessed indices and ensures they're not
+ * repeated, so that it is possible to walk more than one permutation of the
+ * same image.
+ */
+public class ImagePermuter {
+
+  /**
+   * The permutation in use.
+   */
+  private Permutation permutation;
+
+  /**
+   * The coefficient accessor we're permuting.
+   */
+  private CoefficientAccessor accessor;
+
+  /**
+   * The coefficients that have already been visited.
+   */
+  private BitSet locked;
+
+  /**
+   * Given a coefficient accessor, build a Permutation of its indices,
+   * suitable to be used in an ImagePermuter instance.
+   * It is returned uninitialized.
+   * @param r the random number generator for the permutation.
+   * @param acc the accessor.
+   * @return a permutation of the indices in it.
+   */
+  public static Permutation buildPermutation(Random r,
+      CoefficientAccessor acc) {
+    return new Permutation(acc.getLength(), r);
+  }
+
+  /**
+   * CTOR.
+   * @param acc the coefficient accessor to use.
+   * @param p the permutation of its indices.
+   */
+  public ImagePermuter(CoefficientAccessor acc, Permutation p) {
+    accessor = acc;
+    permutation = p;
+    locked = new BitSet(acc.getLength());
+  }
+
+  /**
+   * Walk the permuted image, running the procedure given on every good
+   * coefficient found (ie every non zero, non one, non DC, coefficient).
+   * The procedure's first argument is the index, and its second argument is
+   * the value.
+   * @param proc the procedure to run.
+   */
+  public void walk(TIntIntProcedure proc) {
+    boolean go = true;
+    while(go && permutation.hasNext()) {
+      int index = permutation.next();
+      /* getCoefficient is a native call, so we want to avoid it if possible,
+       * which is why we ensure the coefficient is not DC before going any
+       * further */
+      if(!locked.get(index) && accessor.isDC(index) == 0) {
+        int value = accessor.getCoefficient(index);
+        if(value != 0) {
+          locked.set(index);
+          go = proc.execute(index, value);
+        }
+      }
+    }
+  }
+}
---
+++ src/test/java/com/stegosaurus/steganographers/ImagePermuterTest.java
@@ -0,0 +1,90 @@
+package com.stegosaurus.steganographers;
+
+import gnu.trove.procedure.TIntIntProcedure;
+import gnu.trove.set.TIntSet;
+import gnu.trove.set.hash.TIntHashSet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+import static org.junit.Assume.assumeNoException;
+import static org.junit.Assert.assertFalse;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.stegosaurus.cpp.CoefficientAccessor;
+import com.stegosaurus.cpp.JPEGContext;
+import com.stegosaurus.cpp.JPEGImage;
+import com.stegosaurus.crypt.Permutation;
+import com.stegosaurus.stegutils.NativeUtils;
+
+/**
+ * Test the image permuter class.
+ * TODO Need a superclass for this and the PM1Test.
+ */
+public class ImagePermuterTest {
+  /**
+   * The jpeg context.
+   */
+  private JPEGContext con;
+
+  /**
+   * The cover image.
+   */
+  private JPEGImage cover;
+
+  /**
+   * Set up a test.
+   */
+  @Before
+  public void setUp() {
+    con = new JPEGContext();
+    InputStream in = getClass().getResourceAsStream("lena-colour.jpeg");
+    try {
+      NativeUtils.StegJoctetArray arr = NativeUtils.readInputStream(in);
+      in.close();
+      cover = con.buildImage(arr.cast(), arr.length());
+    } catch(IOException ioe) {
+      assumeNoException(ioe);
+    }
+  }
+
+  /**
+   * Walk the permuter given, recording any indices in the set given; if
+   * any repeats happen, fail the test.
+   * @param set the set.
+   * @param permuter the permuter.
+   * @param n the number of indices to record.
+   */
+  private void walk(final TIntSet set, ImagePermuter permuter, final int n) {
+    permuter.walk(new TIntIntProcedure() {
+      public boolean execute(int index, int value) {
+        assertFalse("Index " + index + " repeated", set.contains(index));
+        set.add(index);
+        return set.size() < n;
+      }
+    });
+  }
+
+  /**
+   * Test that the permuter does not produce repeat indices.
+   */
+  @Test
+  public void testNoRepeat() {
+    cover.readCoefficients();
+    final TIntSet set = new TIntHashSet();
+    long seed = "The National".hashCode();
+    Random r = new Random(seed);
+    CoefficientAccessor ac = new CoefficientAccessor(cover);
+    Permutation p = ImagePermuter.buildPermutation(r, ac);
+    p.init();
+    ImagePermuter permuter = new ImagePermuter(ac, p);
+    walk(set, permuter, 100);
+    /* Guaranteed to reset the permutation, thus making the ImagePermuter
+     * try to repeat some indices, which is what we want. */
+    r.setSeed(seed);
+    p.init();
+    walk(set, permuter, 100);
+  }
+}
