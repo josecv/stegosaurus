67ea8beaa7f09d863fa5be685009fcb2729b0917
Joe C <escozzia@gmail.com>
2013-09-25 20:26:28 -0400

    A new test for the PMChromosome.
    Tests that the crossover method works out when it's not given an index.

--- stegosaurus-java/src/test/java/com/stegosaurus/steganographers/genetic/PMChromosomeTest.java
+++ stegosaurus-java/src/test/java/com/stegosaurus/steganographers/genetic/PMChromosomeTest.java
@@ -1,6 +1,7 @@
 package com.stegosaurus.steganographers.genetic;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 import java.util.Random;
@@ -70,6 +71,32 @@ public class PMChromosomeTest {
         chromosomes[0].atIndex(i), expected[1].atIndex(i));
       assertEquals(String.format(msg, 1, i),
         chromosomes[1].atIndex(i), expected[0].atIndex(i));
+    }
+  }
+
+  /**
+   * Test the crossover method with a random index.
+   * We can't really know what's going to happen, so we'll just test to make
+   * sure that the changed chromosomes are different from what they were
+   * before.
+   */
+  @Test
+  public void testRandomCrossover() {
+    /* The set up is nearly identical to testCrossover's */
+    PMChromosome[] chromosomes = {
+      new PMChromosome(SIZE, random).randomize(),
+      new PMChromosome(SIZE, random).randomize()
+    };
+    random.setSeed(SEED);
+    PMChromosome[] expected = {
+      new PMChromosome(SIZE, random).randomize(),
+      new PMChromosome(SIZE, random).randomize()
+    };
+    PMChromosome.crossover(chromosomes[0], chromosomes[1]);
+    String msg = "Crossover left chromosome unchanged";
+    for(int i = 0; i < 2; i++) {
+      assertFalse(msg, expected[0].equals(chromosomes[0]));
+      assertFalse(msg, expected[1].equals(chromosomes[1]));
     }
   }
 
