255c00507063d5de317bb2decec7aeba1333e9bb
Joe C <escozzia@gmail.com>
2013-10-19 21:12:20 -0400

    An Individual to help with optimization of the seed used for embedding.
    In particular, it'll try to minimize the number of changes required to
    embed a message into an image. A factory is also included, but no test.
    See previous commit for why the test is missing.

---
+++ stegosaurus-java/src/main/java/com/stegosaurus/steganographers/genetic/SeedChangeCountIndividual.java
@@ -0,0 +1,72 @@
+package com.stegosaurus.steganographers.genetic;
+
+import com.stegosaurus.steganographers.EmbedRequest;
+import com.stegosaurus.steganographers.PM1Embedder;
+
+/**
+ * Uses a chromosome as a seed for a PM1Embedding on a JPEG image, and uses
+ * the number of changes required for the embedding to determine fitness.
+ * Its fitness function is the number of changes required for embedding
+ * over the message length, so that it is always between 0 and 1, and the
+ * more changes are required, the less fit the individual is.
+ */
+public class SeedChangeCountIndividual extends
+  AbstractIndividual<SeedChangeCountIndividual> {
+
+  /**
+   * The embed request to service.
+   */
+  private EmbedRequest request;
+
+  /**
+   * The message we want to embed.
+   */
+  private byte[] message;
+
+  /**
+   * The embedder to use.
+   */
+  private PM1Embedder embedder;
+
+  /**
+   * The number of changes required for embedding (as of the last simulation).
+   */
+  private int changes;
+
+  /**
+   * CTOR.
+   * Note that it receives (among other things) a PM1Embedder. This may use
+   * any PM1Sequence, since the number of changes required to embed an
+   * image is completely independent of the PM sequence used to embed that
+   * image.
+   * @param c the chromosome for this individual. Will be used as a seed.
+   * @param message the message we're interested in embedding.
+   * @param key the key to use for embedding.
+   * @param img the jpeg image we'll be embedding into.
+   * @param embedder the PM1Embedder to use.
+   */
+  public SeedChangeCountIndividual(Chromosome c, EmbedRequest request,
+                                   PM1Embedder embedder) {
+    super(c);
+    this.request = request;
+    this.message = request.getMessage();
+    this.embedder = embedder;
+    changes = message.length;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void simulateImpl() {
+    changes = embedder.fakeEmbed(request, chromosome.asShort());
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected double calculateFitnessImpl() {
+    return ((double) changes) / message.length;
+  }
+}
---
+++ stegosaurus-java/src/main/java/com/stegosaurus/steganographers/genetic/SeedChangeCountIndividualFactory.java
@@ -0,0 +1,44 @@
+package com.stegosaurus.steganographers.genetic;
+
+import java.util.Random;
+
+import com.stegosaurus.steganographers.EmbedRequest;
+import com.stegosaurus.steganographers.PM1Embedder;
+import com.stegosaurus.steganographers.utils.DummyPMSequence;
+
+/**
+ * Builds SeedChangeCountIndividual instances.
+ */
+public class SeedChangeCountIndividualFactory
+  implements IndividualFactory<SeedChangeCountIndividual> {
+
+  /**
+   * The embed request we're servicing.
+   */
+  private EmbedRequest request;
+
+  /**
+   * The embedder.
+   */
+  private PM1Embedder embedder;
+
+  /**
+   * Construct a factory to build individuals used in the optimization of
+   * seeds for a given EmbedRequest.
+   * @param request the embed request to optimize for.
+   * @param factory an embedder factory.
+   * @see SeedChangeCountIndividual for info on the PM1Embedders built here.
+   */
+  public SeedChangeCountIndividualFactory(EmbedRequest request,
+                                          PM1Embedder.Factory factory) {
+    this.embedder = factory.build(new Random(), new DummyPMSequence());
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public SeedChangeCountIndividual build(Chromosome c) {
+    return new SeedChangeCountIndividual(c, request, embedder);
+  }
+}
