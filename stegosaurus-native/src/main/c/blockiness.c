3a25f6357ccf3155edaeb6b206b8ed7f0ccdbb8f
Joe C <escozzia@gmail.com>
2013-11-13 21:58:59 -0500

    A very minor optimization to the blockinessForRow method.
    Prevents the re-calculation of a particular value.

--- stegosaurus-native/src/main/c/blockiness.c
+++ stegosaurus-native/src/main/c/blockiness.c
@@ -5,11 +5,15 @@ int blockinessForRow(int components, int width, JSAMPROW samp_row,
   int index;
   int retval = 0;
   int stride = width * components;
+  /* This represents the index that any given pixel would have if the image
+   * contained a single component. */
+  int index_in_component;
   for(index = 0; index < stride; ++index) {
     if(!(row_index % 8) && row_index) {
       retval += abs(samp_row[index] - previous_row[index]);
     }
-    if(!((index / components) % 8) && (index / components)) {
+    index_in_component = index / components;
+    if(!(index_in_component % 8) && index_in_component) {
       retval += abs(samp_row[index] - samp_row[index - components]);
     }
   }
